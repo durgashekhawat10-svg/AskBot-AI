f7c39a8859cb17fb13fe5a83320c269b47dec621
Durga shekhawat <durgashekhawat10@gmail.com>
2026-07-07 13:02:41 +0530

    Update faq_data.py

--- faq_data.py
+++ faq_data.py
@@ -42,5 +42,32 @@ faq_data = {
         "Big Data refers to extremely large datasets that require advanced processing.",
 
     "What is an Algorithm?":
-        "An algorithm is a step-by-step procedure used to solve a problem."
+        "An algorithm is a step-by-step procedure used to solve a problem.",
+
+    "Is ai helpful in today's era?":
+
+         "Yes, AI is very helpful in today's era because it automates tasks, improves productivity, and supports faster decision-making. It is widely used in education, healthcare, business, and daily life to save time and enhance efficiency."  ,
+    " How does AI work? ":
+         "AI works by using algorithms and data to identify patterns, learn from experience, and make predictions or decisions with minimal human intervention.",
+
+    " What are the benefits of Cloud Computing?":
+         "Cloud computing provides scalability, cost savings, remote access, automatic updates, and secure data backup.",
+
+    "Can AI be used in cloud computing?":
+         "Yes. AI and cloud computing work together to provide intelligent services, automate tasks, analyze large datasets, and improve business operations",
+
+    "Is cloud computing secure?":
+         "Reputable cloud providers use encryption, authentication, and regular security updates to protect data. However, users should also follow good security practices.",
+
+    "What are some real-world applications of AI?":
+         "AI is used in virtual assistants, chatbots, recommendation systems, healthcare, fraud detection, self-driving cars, and language translation.",
+
+    "What are the types of cloud computing?":
+     
+         "The three main types are Public Cloud, Private Cloud, and Hybrid Cloud, each offering different levels of accessibility and control.",
+
+    "Do I need programming knowledge to use AI tools?":
+
+         "No. Many AI tools have user-friendly interfaces that allow beginners to use AI without programming knowledge, although coding is helpful for advanced development."
+
 }
